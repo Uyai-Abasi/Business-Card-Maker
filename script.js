0a69f1253ce465ba62c32d2e833a311974e2d962
Cyrilisaacokon <inyeneobongisaacokon9@gmail.com>
2022-08-18 12:18:27 +0100

    come

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 let filtersOut = document.getElementById("capture")
+let img = document.getElementsByTagName('img')[0]
+let div = document.getElementsByClassName("buttons")
 
+let templates = [...div[0].children]
+
+templates.forEach((element, index)=> {
+    element.addEventListener("click", () =>{
+         
+        if (index == 0) {
+            
+                 img.setAttribute('src', '/pattern.jpg')
+            }
+            
+            
+            else if (index == 1) {
+                img.setAttribute('src', '/ss.jpg' );
+                
+            }
+            else if (index ==2){
+                img.setAttribute('src', 'dogg.jpg');
+
+        }
+    })
+})
+
+
+
+console.log(templates)
 function screenshot(){
 
     html2canvas(document.querySelector("#capture")).then(canvas => {
@@ -7,16 +34,17 @@ function screenshot(){
     });
 }
 
-function change() {
-    filtersOut.setAttribute('src', '/ss.jpg' );
-}
-change()
+// function change() {
+//     img.setAttribute('src', '/patter.jpg')
+// }
 
-function pic (e) {
+
+// function pic () {
+//     img.setAttribute('src', '/ss.jpg' );
     
-}
+// }
 
 
-function pict (e) {
+// function pict (e) {
     
-}
+// }
